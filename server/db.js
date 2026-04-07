702fd3fc236f67bae962adde784bb8a3c445e11a
JING\leewa <leewanjing040501@gmail.com>
2026-04-07 17:14:03 +0800

    add

--- server/db.js
+++ server/db.js
@@ -3,7 +3,10 @@ const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false
+    rejectUnauthorized: false,
+    max: 200,          
+    idleTimeoutMillis: 30000, 
+    connectionTimeoutMillis: 2000, 
   }
 });
 
